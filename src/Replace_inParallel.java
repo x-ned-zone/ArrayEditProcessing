d02411551ec790037c9c8cffbd1763ec6f4ffd10
masixole <mntshinga1992@gmail.com>
2018-11-14 01:33:34 +0200

    change naming conventions

--- src/Replace_inParallel.java
+++ src/Replace_inParallel.java
@@ -13,21 +13,21 @@ public class Replace_inParallel<T> implements Runnable {
 	private T [] array;
 	private T oldValue;
 	private T newValue;
-	private int startPos, endPos;
+	private int startIndex, endIndex;
 
 	Replace_inParallel (T [] array, T oldValue, T newValue, int start, int end) {
 		this.array=array;
 		this.oldValue=oldValue;
 		this.newValue=newValue;
-		this.startPos = start;
-		this.endPos = end;
+		this.startIndex = start;
+		this.endIndex = end;
 	}
 
 	@Override
 	public void run() {
 		try {
 			// Bruteforce: O(n) worst-case.   Auxiliary Space:
-			for (int i = this.startPos; i<this.endPos; i++) {
+			for (int i = this.startIndex; i<this.endIndex; i++) {
 				if (array[i]== oldValue)
 					array[i] = newValue;
 			}
